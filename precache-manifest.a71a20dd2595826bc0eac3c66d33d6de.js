ec6a8edfdc9de8564a0e49a15a420dc39da55168
Illak Zapata <i.zapata1989@gmail.com>
2019-03-14 02:41:36 -0300

    Updates

---
+++ precache-manifest.a71a20dd2595826bc0eac3c66d33d6de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a507f815d930c7c9d30a",
+    "url": "/curso_react_pics/static/css/main.5e2e4ae1.chunk.css"
+  },
+  {
+    "revision": "a507f815d930c7c9d30a",
+    "url": "/curso_react_pics/static/js/main.57366927.chunk.js"
+  },
+  {
+    "revision": "62cae7c917d583cc333a",
+    "url": "/curso_react_pics/static/js/runtime~main.3029bd65.js"
+  },
+  {
+    "revision": "d9d95ad276094ab44227",
+    "url": "/curso_react_pics/static/js/2.126daa44.chunk.js"
+  },
+  {
+    "revision": "57ffc3d39b825e164ecca8df2182d06c",
+    "url": "/curso_react_pics/index.html"
+  }
+];
